bd566af392ba5217994e718fd6cf1cd40e11057a
Deora <umesh.deora@capgemini.com>
2021-02-09 15:56:31 +0530

    [Umesh]Added: paragraph generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { React, useState } from "react";
+import { data } from "./data";
 function App() {
+  const [value, setValue] = useState(0);
+  const [para, setPara] = useState([]);
+  const handleClick = (e) => {
+    e.preventDefault();
+    let amount = parseInt(value);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > data.length) {
+      amount = data.length;
+    }
+    setPara(data.slice(0, amount));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Tired of Boring Lorem Ipsum?</h2>
+      <div>
+        <form onSubmit={handleClick}>
+          <label htmlFor="paragraph">Paragraphs:</label>
+          <input
+            type="number"
+            id="paragraph"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">Generate</button>
+        </form>
+        {para.map((paragraph) => {
+          return <p key={paragraph.id}>{paragraph.desc}</p>;
+        })}
+      </div>
     </div>
   );
 }
